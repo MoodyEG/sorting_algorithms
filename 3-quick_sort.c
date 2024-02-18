8d1764a4088f3e43ed8b662af8be668041410bea
MoodyEG <moodyeg2011@yahoo.com>
2024-02-17 20:20:43 -0800

    Adding quick sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,53 @@
+#include "sort.h"
+
+/**
+ * quick_sort - Sorts using the Quick sort algorithm
+ * @array: Out array to sort
+ * @size: The size of the array
+ * Return: No return
+ */
+ void quick_sort(int *array, size_t size)
+ {
+	_quick(array, size, 0, size - 1);
+ }
+/**
+ * _quick - Sorts using the Quick sort algorithm
+ * @array: Out array to sort
+ * @size: The size of the array
+ * @left: the left side
+ * @right: the right side
+ * Return: No return
+ */
+ void _quick(int *array, size_t size, int left, int right)
+ {
+	int i, j, temp;
+
+	if (left < right)
+	{
+		i = left;
+		for (j = left; j < right; j++)
+		{
+			if (array[j] < array[right])
+			{
+				if (j != i)
+				{
+					temp = array[j];
+					array[j] = array[i];
+					array[i] = temp;
+					print_array(array, size);
+				}
+				i++;
+			}
+		}
+		if (i != right)
+		{
+			temp = array[right];
+			array[right] = array[i];
+			array[i] = temp;
+			print_array(array, size);
+		}
+		_quick(array, size, left, i - 1);
+		_quick(array, size, i + 1, right);
+	}
+ }
+
