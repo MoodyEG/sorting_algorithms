8cedc017cfc07d2f693c8270455bd2c61bd6aa17
MoodyEG <moodyeg2011@yahoo.com>
2024-02-17 17:00:16 -0800

    Adding Bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Sorts using the Bubble sort algorithm
+ * @array: Out array to sort
+ * @size: The size of the array
+ * Return: No return
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i = 0, n;
+	int temp;
+
+	n = size;
+	while (n)
+	{
+		for (i = 0; i < size - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	n--;
+	}
+}
