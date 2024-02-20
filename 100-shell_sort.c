d54dbed4c1d413289a3cbb7f461a39ae28a37a54
MoodyEG <moodyeg2011@yahoo.com>
2024-02-19 20:48:03 -0800

    Adding Shell sort

---
+++ 100-shell_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * shell_sort - Sorts using the Shell sort algorithm
+ * @array: Out array to sort
+ * @size: The size of the array
+ * Return: No return
+ */
+void shell_sort(int *array, size_t size)
+{
+	size_t gap = 1, i, j;
+	int temp;
+
+	while (gap * 3 + 1 < size)
+		gap = gap * 3 + 1;
+	while (gap > 0)
+	{
+		for (i = gap; i < size; i++)
+		{
+			for (j = i; j > gap - 1 ; j -= gap)
+			{
+				if (array[j] < array[j - gap])
+				{
+					temp = array[j];
+					array[j] = array[j - gap];
+					array[j - gap] = temp;
+				}
+			}
+		}
+		print_array(array, size);
+		gap = (gap - 1) / 3;
+	}
+}
